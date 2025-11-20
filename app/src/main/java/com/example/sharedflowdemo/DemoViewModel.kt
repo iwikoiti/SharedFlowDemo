cffa6f2717a4d38098a7519ce2da05e95d428a6f
yui <yuineor@gmail.com>
2025-11-20 14:03:38 +0700

    instance launch SharedFlow

--- app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
+++ app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.sharedflowdemo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class DemoViewModel: ViewModel() {
+    //Изменяемый поток
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    //Только для чтения, версия потока
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    //Когда создается ViewModel, сразу вызывается
+    init {
+        sharedFlowInit()
+    }
+
+    //Функция запуска корутины (корутина отменяется, когда VM уничтожается)
+    private fun sharedFlowInit() {
+        viewModelScope.launch {
+            for (i in 1..1000) {
+                delay(2000)
+                _sharedFlow.emit(i)
+            }
+        }
+    }
 }
